dfbb9cf9d500a0b536b69737dea210433722d6a5
rafacollioni <rafa.collioni@gmail.com>
2022-09-22 21:53:47 -0300

    TERMINADO

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
 let numBtn = document.querySelectorAll('.btn');
 
+// Event Handlers dos Clicks
+
 for (let i = 0; i < numBtn.length; i++){
    
-    document.querySelectorAll('.btn')[i].addEventListener('click', function (){
-        switch (i){
-            case 0:
-                let crash = new Audio('/sounds/crash.mp3')
-	            crash.play();
-            break;
-            case 1:
-                let kick= new Audio('/sounds/kick-bass.wav')
-	            kick.play();
-            break;
-            case 2:
-                let snare = new Audio('/sounds/snare.mp3')
-	            snare.play();
-            break;
-            case 3:
-                let tom1 = new Audio('/sounds/tom-1.mp3')
-	            tom1.play();
-            break;
-            case 4:
-                let tom2 = new Audio('/sounds/tom-2.mp3')
-	            tom2.play();
-            break;
-            case 5:
-                let tom3 = new Audio('/sounds/tom-3.mp3')
-	            tom3.play();
-            break;
-            case 6:
-                let tom4 = new Audio('/sounds/tom-4.mp3')
-	            tom4.play();
-            break;
-        }
+    document.querySelectorAll('.btn')[i].addEventListener('click', function(){
+        let clickButton = this.innerText
+        drumPlay(clickButton)
     })
 }
+
+// Event Handlers das Teclas   
+
+document.addEventListener("keydown", function(event){
+    let tecla = event.key.toUpperCase();
+    drumPlay(tecla);
+})
+
+// Função Principal para Reproduzir os Sons
+
+function drumPlay(buttonInnerHTML){
+
+    switch (buttonInnerHTML){
+        case "A":
+            let crash = new Audio('/sounds/crash.mp3')
+            crash.play();
+        break;
+        case "S":
+            let kick= new Audio('/sounds/kick-bass.wav')
+            kick.play();
+        break;
+        case "D":
+            let snare = new Audio('/sounds/snare.mp3')
+            snare.play();
+        break;
+        case "H":
+            let tom1 = new Audio('/sounds/tom-1.mp3')
+            tom1.play();
+        break;
+        case "J":
+            let tom2 = new Audio('/sounds/tom-2.mp3')
+            tom2.play();
+        break;
+        case "K":
+            let tom3 = new Audio('/sounds/tom-3.mp3')
+            tom3.play();
+        break;
+        case "L":
+            let tom4 = new Audio('/sounds/tom-4.mp3')
+            tom4.play();
+        break;
+    }
+
+}
+
+
+
+
+
+
 
